cda8b1b5fceaf00a132232fd55d7d72de605e549
Michael D'Auria <michael.dauria@gmail.com>
2010-08-11 22:43:56 -0400

    Config for chef-solo

---
+++ config/solo.rb
@@ -0,0 +1,6 @@
+log_level        :info
+log_location     STDOUT
+ssl_verify_mode  :verify_none
+cookbook_path    ["/tmp/chef/recipes/cookbooks"]
+file_store_path  "/tmp/chef/recipes/"
+file_cache_path  "/tmp/chef/recipes/"
