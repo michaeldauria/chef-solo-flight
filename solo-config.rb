b63171e2e838dde3b02e72561131d1a1903d6484
Michael D'Auria <michael.dauria@gmail.com>
2010-08-31 23:14:11 -0400

    Move chef-solo config location

---
+++ solo-config.rb
@@ -0,0 +1,6 @@
+cookbook_path    [ '/tmp/chef/recipes/cookbooks', '/tmp/chef/recipes/site-cookbooks' ]
+file_cache_path  '/tmp/chef/recipes/'
+file_store_path  '/tmp/chef/recipes/'
+log_level        :info
+log_location     STDOUT
+ssl_verify_mode  :verify_none
