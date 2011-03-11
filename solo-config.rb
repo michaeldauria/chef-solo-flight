fab9d59e9aaa9b52c77f294c6e749a30c2adc546
Michael D'Auria <michael.dauria@gmail.com>
2011-03-11 17:36:27 -0500

    Support any number of cookbook folders

--- solo-config.rb
+++ solo-config.rb
@@ -1,6 +1,14 @@
-cookbook_path    [ '/tmp/chef/recipes/cookbooks', '/tmp/chef/recipes/site-cookbooks' ]
-file_cache_path  '/tmp/chef/recipes/'
-file_store_path  '/tmp/chef/recipes/'
+chef_base = '/tmp/chef/recipes/'
+
+chef_cookbook_dirs = []
+Dir.entries(chef_base).each do |d|
+  next if d[0,1] == '.'
+  chef_cookbook_dirs << '%s%s' % [ chef_base, d ]
+end
+
+cookbook_path    chef_cookbook_dirs
+file_cache_path  chef_base
+file_store_path  chef_base
 log_level        :info
 log_location     STDOUT
 ssl_verify_mode  :verify_none
