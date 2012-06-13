d5dffb643de18d1d72a02b7fe3ae7e3cc6c9bb9e
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-06-13 15:33:20 -0400

    Added support for roles

--- solo-config.rb
+++ solo-config.rb
@@ -1,14 +1,16 @@
-chef_base = '/tmp/chef/recipes/'
+chef_base = '/tmp/chef/'
+chef_recipes = chef_base + 'recipes/'
 
 chef_cookbook_dirs = []
-Dir.entries(chef_base).each do |d|
+Dir.entries(chef_recipes).each do |d|
   next if d[0,1] == '.'
-  chef_cookbook_dirs << '%s%s' % [ chef_base, d ]
+  chef_cookbook_dirs << '%s%s' % [ chef_recipes, d ]
 end
 
 cookbook_path    chef_cookbook_dirs
-file_cache_path  chef_base
-file_store_path  chef_base
+role_path        chef_base + 'roles/'
+file_cache_path  chef_recipes
+file_store_path  chef_recipes
 log_level        :info
 log_location     STDOUT
 ssl_verify_mode  :verify_none
